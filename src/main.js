094f26dccd65fd4a90a3a1f1fe945ac21e0cff41
RedSparr0w <admin@redsparr0w.com>
2020-08-12 17:45:26 +1200

    initial test

---
+++ src/main.js
@@ -0,0 +1,84 @@
+'use strict';
+
+/* eslint-disable no-console */
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+const DiscordRPC = require('discord-rpc');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 340,
+    height: 380,
+    resizable: false,
+    titleBarStyle: 'hidden',
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true,
+  }));
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// Set this to your Client ID.
+const clientId = '733927271726841887';
+
+// Only needed if you want to use spectate, join, or ask to join
+DiscordRPC.register(clientId);
+
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+const startTimestamp = new Date();
+
+async function setActivity() {
+  if (!rpc || !mainWindow) {
+    return;
+  }
+
+  const boops = await mainWindow.webContents.executeJavaScript('window.boops');
+
+  // You'll need to have snek_large and snek_small assets uploaded to
+  // https://discord.com/developers/applications/<application_id>/rich-presence/assets
+  rpc.setActivity({
+    details: `${boops}/151 ✨`,
+    state: 'Breeding Pokemon',
+    // largeImageKey: 'snek_large',
+    // largeImageText: 'tea is delicious',
+    // smallImageKey: 'snek_small',
+    // smallImageText: 'i am my own pillows',
+    instance: false,
+  });
+}
+
+rpc.on('ready', () => {
+  setActivity();
+
+  // activity can only be set every 15 seconds
+  setInterval(() => {
+    setActivity();
+  }, 15e3);
+});
+
+rpc.login({ clientId }).catch(console.error);
---
+++ src/renderer.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/* eslint-env browser */
+
+const { webFrame } = require('electron');
+
+const snek = document.getElementById('snek');
+const counter = document.getElementById('boops');
+
+window.boops = 0;
+function boop() {
+  window.boops += 1;
+  counter.innerHTML = `${window.boops} BOOPS`;
+}
+
+snek.onmousedown = () => {
+  snek.style['font-size'] = '550%';
+  boop();
+};
+
+snek.onmouseup = () => {
+  snek.style['font-size'] = '500%';
+};
